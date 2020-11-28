7f99dbfd26a11b4814ac92cee998fd4dace1125b
Damir Pristav <damirpristav@gmail.com>
2020-11-28 09:52:42 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+import Start from './components/Start';
+import Question from './components/Question';
+import End from './components/End';
+import Modal from './components/Modal';
+import quizData from './data/quiz.json';
+
+let interval;
+
+const App = () => {
+  const [step, setStep] = useState(1);
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    if(step === 3) {
+      clearInterval(interval);
+    }
+  }, [step]);
+
+  const quizStartHandler = () => {
+    setStep(2);
+    interval = setInterval(() => {
+      setTime(prevTime => prevTime + 1);
+    }, 1000);
+  }
+
+  const resetClickHandler = () => {
+    setActiveQuestion(0);
+    setAnswers([]);
+    setStep(2);
+    setTime(0);
+    interval = setInterval(() => {
+      setTime(prevTime => prevTime + 1);
+    }, 1000);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {step === 1 && <Start onQuizStart={quizStartHandler} />}
+      {step === 2 && <Question 
+        data={quizData.data[activeQuestion]}
+        onAnswerUpdate={setAnswers}
+        numberOfQuestions={quizData.data.length}
+        activeQuestion={activeQuestion}
+        onSetActiveQuestion={setActiveQuestion}
+        onSetStep={setStep}
+      />}
+      {step === 3 && <End 
+        results={answers}
+        data={quizData.data}
+        onReset={resetClickHandler}
+        onAnswersCheck={() => setShowModal(true)}
+        time={time}
+      />}
+
+      {showModal && <Modal 
+        onClose={() => setShowModal(false)}
+        results={answers}
+        data={quizData.data}
+      />}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bulma/css/bulma.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
